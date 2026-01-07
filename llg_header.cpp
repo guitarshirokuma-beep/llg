616fe228ac177ffa038afc440154a5ab9714496b
Seikuu Okada <guitar.shirokuma@au.com>
2026-01-07 12:59:17 +0900

    divide def_Make2DArray.cpp

--- llg_header.cpp
+++ llg_header.cpp
@@ -6,27 +6,6 @@
 #include"llg.hpp"
 using namespace std;
 
-Vec3& Make2DArray::operator()(int x, int t){
-			return val[x * N_steps + t];
-		}
-
-const Vec3& Make2DArray::operator()(int x, int t) const{
-	return val[x * N_steps + t];
-}
-
-Make2DArray operator/(
-	const Make2DArray& a,
-	const Make2DArray& b
-){
-	Make2DArray result(a.Lx, a.N_steps);
-	for(int n=0; n<a.Lx; n++){
-		for(int step=0; step<a.N_steps; step++){
-			result(n, step) = a(n, step) / b(n, step);
-		}
-	}
-	return result;
-}
-
 Make1DArray extract_const_step(
 	const Params&		p,
 	const Make2DArray&	S_2d,
