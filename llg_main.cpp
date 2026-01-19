9fb0c1f2524d7ae517ef29b0fa50384473f945dc
Seikuu Okada <guitar.shirokuma@au.com>
2026-01-19 14:04:57 +0900

    'axis' -> [](Vec3& v){return v.axis;} for Make2DArray fft_2d

--- llg_main.cpp
+++ llg_main.cpp
@@ -23,9 +23,9 @@ int main(){
 	Make2DArray h_app(p.Lx, p.N_steps);
 	initialize(p, S, h_app);
 	run_llg(p, S, h_app);
-	S = fft_2d(p, S, 'y');
+	S = fft_2d(p, S, [](const Vec3& v){return v.y;}, [](Vec3& v, double val){v.y = val;});
 	Make2DArray response(p.Lx, p.N_steps);
-	h_app = fft_2d(p, h_app, 'x');
+	h_app = fft_2d(p, h_app, [](const Vec3& v){return v.x;}, [](Vec3& v, double val){v.x = val;});
 	avoid_zero(p, h_app);
 	response = calc_response(p, S, h_app);
 	output_data(p, response, 'x');
