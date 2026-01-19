e2475b2e32d82c7cad6bd87745c3c998e0f539b0
Seikuu Okada <guitar.shirokuma@au.com>
2026-01-19 17:30:45 +0900

    reshape llg_main.cpp

--- llg_main.cpp
+++ llg_main.cpp
@@ -24,11 +24,34 @@ int main(){
 	Make2DArray h_app(p.Lx, p.N_steps);
 	initialize(p, S, h_app);
 	run_llg(p, S, h_app);
-	S = fft_2d(p, S, [](const Vec3& v){return v.y;}, [](Vec3& v, double val){v.y = val;});
+
+	S = fft_2d(
+		p,
+		S,
+		[](const Vec3& v){return v.y;},
+		[](Vec3& v, double val){v.y = val;}
+	);
+
 	Make2DArray response(p.Lx, p.N_steps);
-	h_app = fft_2d(p, h_app, [](const Vec3& v){return v.x;}, [](Vec3& v, double val){v.x = val;});
+
+	h_app = fft_2d(
+		p,
+		h_app,
+		[](const Vec3& v){return v.x;},
+		[](Vec3& v, double val){v.x = val;}
+	);
+
 	avoid_zero(p, h_app);
-	response = calc_response(p, S, [](const Vec3& v){ return v.y;}, h_app, [](const Vec3& v){return v.x;});
+
+	response = calc_response(
+		p,
+		S,
+		[](const Vec3& v){ return v.y;},
+		h_app,
+		[](const Vec3& v){return v.x;}
+	);
+
 	output_data(p, response, 'x');
+	
 	return 0;
 }
