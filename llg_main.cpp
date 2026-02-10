73878d16635debe29accab3464c1965f1f877a23
Seikuu Okada <guitar.shirokuma@au.com>
2026-02-10 16:24:17 +0900

    make run_utils.hpp &  save_params()

--- llg_main.cpp
+++ llg_main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
 	const int Lx = 2048;
-	const int N_steps = 16384;
+	const int N_steps = 2048;
 	const double lam = 0.03;
 	const double h_app_norm = 10.0;
 	const double sigma_x = 0.5;
--- run_utils.hpp
+++ run_utils.hpp
@@ -2,6 +2,7 @@
 #include <chrono>
 #include <iomanip>
 #include <sstream>
+#include "params.hpp"
 
 std::string make_run_dir()
 {
@@ -13,4 +14,25 @@ std::string make_run_dir()
 
     std::filesystem::create_directories(ss.str());
     return ss.str();
+}
+
+#include <fstream>
+
+void save_params(const Params &p, const std::string &dir)
+{
+    std::ofstream f(dir + "/params.txt");
+
+    f << "Lx " << p.Lx << "\n";
+    f << "N_steps " << p.N_steps << "\n";
+    f << "lam " << p.lam << "\n";
+    f << "h_app_norm " << p.h_app_norm << "\n";
+    f << "sigma_x " << p.sigma_x << "\n";
+    f << "sigma_step " << p.sigma_step << "\n";
+    f << "pulse_norm " << p.pulse_norm << "\n";
+    f << "time_pulse_center " << p.time_pulse_center << "\n";
+    f << "local_pulse_center " << p.local_pulse_center << "\n";
+    f << "J " << p.J << "\n";
+    f << "delta " << p.delta << "\n";
+    f << "dt " << p.dt << "\n";
+    f << "gamma " << p.gamma << "\n";
 }
