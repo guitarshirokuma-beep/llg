ca932262bb0c26314a3e32c597ef1cdb14a551e0
Seikuu Okada <guitar.shirokuma@au.com>
2026-02-10 18:08:52 +0900

    remake params.hpp

--- llg_main.cpp
+++ llg_main.cpp
@@ -8,19 +8,7 @@ using namespace std;
 
 int main()
 {
-	const int Lx = 2048;
-	const int N_steps = 2048;
-	const double lam = 0.03;
-	const double h_app_norm = 10.0;
-	const double sigma_x = 0.5;
-	const double sigma_step = 0.8;
-	const double pulse_norm = 0.01;
-	const double time_pulse_center = N_steps / 2.0;
-	const double local_pulse_center = Lx / 2.0;
-	const double J = 10.0;
-	const double delta = 1e-13; // for avoid_zero()
-
-	Params p(Lx, N_steps, lam, h_app_norm, sigma_x, sigma_step, pulse_norm, time_pulse_center, local_pulse_center, J, delta);
+	Params p;
 
 	// mkdir for log
 	auto run_dir = make_run_dir();
--- params.hpp
+++ params.hpp
@@ -4,42 +4,19 @@
 struct Params
 {
 public:
-	int Lx;
-	int N_steps;
-	double lam;
-	double h_app_norm;
-	double sigma_x;
-	double sigma_step;
-	double pulse_norm;
-	double time_pulse_center;
-	double local_pulse_center;
-	double delta;
+	const int Lx = 2048;
+	const int N_steps = 2048;
+	const double lam = 0.03;
+	const double h_app_norm = 10.0;
+	const double sigma_x = 0.5;
+	const double sigma_step = 0.8;
+	const double pulse_norm = 0.01;
+	const double time_pulse_center = double(N_steps) / 2.0;
+	const double local_pulse_center = double(Lx) / 2.0;
+	const double delta = 1e-13; // for avoid_zero()
 	const double dt = 0.001;
 	const double gamma = 1.0;
-	double J;
-	Params(
-		int Lx_,
-		int N_steps_,
-		double lam_,
-		double h_app_norm_,
-		double sigma_x_,
-		double sigma_step_,
-		double pulse_norm_,
-		double time_pulse_center_,
-		double local_pulse_center_,
-		double J_,
-		double delta_)
-		: Lx(Lx_),
-		  N_steps(N_steps_),
-		  lam(lam_),
-		  h_app_norm(h_app_norm_),
-		  sigma_x(sigma_x_),
-		  sigma_step(sigma_step_),
-		  pulse_norm(pulse_norm_),
-		  time_pulse_center(time_pulse_center_),
-		  local_pulse_center(local_pulse_center_),
-		  J(J_),
-		  delta(delta_) {};
+	const double J = 10.0;
 };
 
 #endif
